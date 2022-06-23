57942e314d9dea62bd950971a619b05d1214d2ee
Genildo <genildocs@gmail.com>
2022-06-23 05:06:51 -0300

    OperadoresLogicos

---
+++ src/OperadoresLogicos.java
@@ -0,0 +1,27 @@
+public class OperadoresLogicos {
+    public static void main(String[] args) {
+
+         /*Operador logico AND (&&) Retorna true se duas condições logicas forem verdadeiras.
+        Operador logico OR (||) Retorna true se uma ou as duas  condições logicas forem verdadeiras e false se as
+        duas condições forem falsas.
+        Operador OR (!) operador de negação logica, se é verdade retorna falso e vice-versa.
+         */
+
+        int idade = 18;
+        float salario = 2500F;
+
+        boolean negacaoLogica = !(idade > 30);
+        System.out.println("Negação logica retornou: " + negacaoLogica + " , mesmo sendo uma condição falsa.");
+
+        if (idade == 18 && salario == 2500) {
+            System.out.println("Verdade, pois as duas condições são verdadeiras.");
+        }
+
+        if (idade != 18 || salario != 2500) {
+            System.out.println("Verdade, caso uma ou as duas condições sejam verdadeiras.");
+        }else{
+            System.out.println("Falso, caso as duas condições sejam falsa.");
+        }
+
+    }
+}
