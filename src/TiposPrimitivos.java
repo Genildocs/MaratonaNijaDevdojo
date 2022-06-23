2185fe2f08713a093013c509270ff64e10cba49f
Genildo <genildocs@gmail.com>
2022-06-23 03:11:31 -0300

    Primeiro commit

---
+++ src/TiposPrimitivos.java
@@ -0,0 +1,26 @@
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        // int, double, float, char, byte, short, long, boolean, string
+        int idade = 10;
+        long numeroGrande = 1000000;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500.0F;
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        boolean verdade = true;
+        boolean falso = false;
+        char caractere = 'M';
+        String nome = "Genildo"; // No java string é uma classe e não um dado do tipo primitivo.
+
+        System.out.println(idade);
+        System.out.println(numeroGrande);
+        System.out.println(salarioDouble);
+        System.out.println(salarioFloat);
+        System.out.println(idadeByte);
+        System.out.println(idadeShort);
+        System.out.println(verdade);
+        System.out.println(falso);
+        System.out.println(caractere);
+        System.out.println(nome);
+    }
+}
